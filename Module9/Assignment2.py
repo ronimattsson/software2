63b513fe06ae0810295d80a72410cca10a2b10e2
Roni Mattsson <mattsson.roni@gmail.com>
2026-02-22 23:11:26 +0200

    Auton nopeuden muuttaminen

---
+++ Module9/Assignment2.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.current_speed = 0
+        self.travelled_distance = 0
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+
+    def accelerate(self, speed):
+        self.current_speed += speed
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed <= 0:
+            self.current_speed = 0
