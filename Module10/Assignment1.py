283e12f70ccbf945442f602877aa71dda1c4c4c7
Roni Mattsson <mattsson.roni@gmail.com>
2026-02-27 17:36:36 +0200

    Hissin alaspäin kutsu korjattu

--- Module10/Assignment1.py
+++ Module10/Assignment1.py
@@ -1,14 +1,18 @@
 class Elevator:
     def __init__(self, low, high):
-        self.floor = int(low)
+        self.current_floor = int(low)
         self.bottom_floor = int(low)
         self.top_floor = int(high)
-        def go_to_floor(self, target_floor):
-            for i in range (target_floor):
-                floor_up()
-        def floor_up(self):
-            if self.floor < self.highest:
-                self.floor += 1
-        def floor_down(self):
-            if self.floor > self.lowest:
-                self.floor -= 1
+    def floor_up(self):
+        if self.current_floor < self.top_floor:
+            self.current_floor += 1
+    def floor_down(self):
+        if self.current_floor > self.bottom_floor:
+            self.current_floor -= 1
+    def go_to_floor(self, target_floor):
+        steps = (self.current_floor - target_floor).__abs__()
+        for i in range (steps):
+            if target_floor < self.current_floor:
+                self.floor_down()
+            elif target_floor > self.current_floor:
+                self.floor_up()
