71df52cce5d61fd95751f1b9f600c5feab1b5f36
Roni Mattsson <mattsson.roni@gmail.com>
2026-02-27 19:27:44 +0200

    Aliluokkia Car luokalle

---
+++ Module11/Assignment2.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.current_speed = 0
+        self.travelled_distance = 0.0
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+
+    def accelerate(self, speed):
+        self.current_speed += speed
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed <= 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance += int(hours) * int(self.current_speed)
+
+class ElectricCar(Car):
+    def __init__(self, license_plate, maximum_speed, battery_capacity):
+        super().__init__(license_plate, maximum_speed)
+        self.battery_capacity = battery_capacity
+
+class GasolineCar(Car):
+    def __init__(self, license_plate, maximum_speed, tank_volume):
+        super().__init__(license_plate, maximum_speed)
+        self.tank_volume = tank_volume
